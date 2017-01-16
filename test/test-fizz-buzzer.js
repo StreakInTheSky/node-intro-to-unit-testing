f0b0383b9e722df4511779ee8616613e0b52c39a
StreakInTheSky <streakinthesky@gmail.com>
2017-01-16 00:16:31 -0800

    create tester for fizz

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -1,11 +1,15 @@
 const should = require('chai').should();
 
-const adder = require('../fizzBuzzer');
+const fizzBuzzer = require('../fizzBuzzer');
 
 describe('adder', function () {
 	it('should return fizz', function() {
-
-	})
+		const fizzCases = [3,6,9,12,18,21];
+		fizzCases.forEach(function(input) {
+			const response = fizzBuzzer(input);
+			response.should.equal('fizz');
+		});
+	});
 
 	it('should return buzz', function() {
 
@@ -15,7 +19,7 @@ describe('adder', function () {
 
 	})
 
-	it('should throw error'), function() {
+	it('should throw error', function() {
 
-	}}
+	})
 })
