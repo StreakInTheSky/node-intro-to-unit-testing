c0b5ee79856a8ba5d092818a26f806ca808fbe1b
StreakInTheSky <streakinthesky@gmail.com>
2017-01-15 23:37:26 -0800

    create fuzzBuzzer test file

---
+++ test/test-fizz-buzzer.js
@@ -0,0 +1,21 @@
+const should = require('chai').should();
+
+const adder = require('../fizzBuzzer');
+
+describe('adder', function () {
+	it('should return fizz', function() {
+
+	})
+
+	it('should return buzz', function() {
+
+	})
+
+	it('should return fizz-buzz', function() {
+
+	})
+
+	it('should throw error'), function() {
+
+	}}
+})
