1033be0ce0d3c340e9a472b78a142d6642efdded
StreakInTheSky <streakinthesky@gmail.com>
2017-01-18 19:00:17 -0800

    add all other tests

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -2,24 +2,42 @@ const should = require('chai').should();
 
 const fizzBuzzer = require('../fizzBuzzer');
 
-describe('adder', function () {
+describe('fizzBuzzer', function () {
 	it('should return fizz', function() {
 		const fizzCases = [3,6,9,12,18,21];
-		fizzCases.forEach(function(input) {
+		fizzCases.forEach(input => {
 			const response = fizzBuzzer(input);
 			response.should.equal('fizz');
 		});
 	});
 
 	it('should return buzz', function() {
-
+		const buzzCases = [5,10,20,25,35];
+		buzzCases.forEach(input => {
+			const response = fizzBuzzer(input);
+			response.should.equal('buzz');
+		})
 	})
 
 	it('should return fizz-buzz', function() {
-
+		const fizzBuzzCases = [15,30,45,60,75];
+		fizzBuzzCases.forEach(input => {
+			const response = fizzBuzzer(input);
+			response.should.equal('fizz-buzz');
+		})
+	})
+	it('should return the input', function() {
+		const numCases = [1,2,4,7,8];
+		numCases.forEach(input => {
+			const response = fizzBuzzer(input);
+			response.should.equal(input);
+		})
 	})
 
 	it('should throw error', function() {
-
+		const notNumCases = [false, 'hello', [1,2]];
+		notNumCases.forEach(input => {
+			(function() {fizzBuzzer(input)}).should.throw(Error);
+		})
 	})
 })
